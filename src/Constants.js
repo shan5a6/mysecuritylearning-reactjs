32067d67c88e386cd8b23de8b269fa114831c4da
root <root@ip-172-31-18-92.ec2.internal>
2024-09-16 06:23:45 +0000

    update java packages

---
+++ src/Constants.js
@@ -0,0 +1,8 @@
+import  {Component} from 'react';
+
+class Constants extends Component {
+  static backendUrlBase = "http://35.204.241.43:31337/";
+  static backendRESTUrlBase = "http://35.204.241.43:31337/rest/";
+}
+
+export default Constants;
